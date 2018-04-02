05334f9780877e9a49d73268f75bd04c0d018053
kangbinbin <535755455@qq.com>
2018-04-02 10:23:44 +0800

    添加pod

--- BaseProject/BaseProject/Classes/Home/Controller/KHomeViewController.m
+++ BaseProject/BaseProject/Classes/Home/Controller/KHomeViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "KHomeViewController.h"
-
+//#import "MJExtension.h"
 @interface KHomeViewController ()
 
 @end
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
 }
 
@@ -25,14 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
